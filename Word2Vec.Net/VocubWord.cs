5112406cee32255e3fcd2c47eb18991656548811
kornev-b <kornev.b@gmail.com>
2015-08-03 02:22:49 +0300

    #new

---
+++ Word2Vec.Net/VocubWord.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace Word2Vec.Net
+{
+    internal class VocubWord
+    {
+        public long Cn { get; set; }
+        public string Word { get; set; }
+        public char[] Code { get; set; }
+        public char CodeLen { get; set; }
+        public int[] Point { get; set; }
+    }
+    // Used later for sorting by word counts
+    internal class VocubComparer : IComparer<VocubWord>
+    {
+        public int Compare(VocubWord x, VocubWord y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (y == null) return -1;
+            if (x == null) return 1;
+            return (int)(y.Cn - x.Cn );
+        }
+    }
+}
